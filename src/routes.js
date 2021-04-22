658804f88cff4a3f7153d5895e5bc3b8bfe6d79f
Ecthon Borhis <ecthon@gmail.com>
2021-04-22 16:19:05 -0400

    Created page Job

--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,6 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => res.render("index"))
+routes.get('/job', (req, res) => res.render("job"))
 
 module.exports = routes;
