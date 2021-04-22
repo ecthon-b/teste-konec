e814b24dc557ab51daa3e0b3a67fcc922fb3d479
Ecthon Borhis <ecthon@gmail.com>
2021-04-22 15:48:34 -0400

    Created page index

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
-const express = require('express')
+const express = require("express")
 const server = express()
-
+const routes = require("./routes")
+const path = require("path")
 
 server.set('view engine', 'ejs')
 server.use(express.static("public"))
-server.listen(3000)
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(routes)
+
+server.listen(3000, () => console.log('rodando'))
