0e1a82adc4a0a8906ad4199549c35d5fb32567d1
Ecthon Borhis <ecthon@gmail.com>
2021-04-23 17:43:58 -0400

    Created controllers

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const routes = express.Router()
 
+const Job = {
+    controller: {
+        index(req, res) {
+
+        }
+    },
+    services: {
+
+    }
+}
+
+// routes.get('/', Job.controllers.index)
 routes.get('/', (req, res) => res.render("index"))
 routes.get('/job', (req, res) => res.render("job"))
+routes.post('/job', (req, res) => res.render("job"))
 routes.get('/job/edit', (req, res) => res.render("job-edit"))
 
 
